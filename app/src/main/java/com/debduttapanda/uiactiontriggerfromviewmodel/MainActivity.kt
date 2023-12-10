05f2df9dab99d04410625470bbde80bb932afb19
Debdutta Panda <me@debduttapanda.com>
2023-12-10 17:42:26 +0530

    example code

--- app/src/main/java/com/debduttapanda/uiactiontriggerfromviewmodel/MainActivity.kt
+++ app/src/main/java/com/debduttapanda/uiactiontriggerfromviewmodel/MainActivity.kt
@@ -6,12 +6,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.debduttapanda.uiactiontriggerfromviewmodel.ui.theme.UIActionTriggerFromViewModelTheme
@@ -29,48 +31,48 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MyPage()
+                    val vm: MyViewModel = viewModel()
+                    Column(){
+                        Button(onClick = {
+                            vm.onButtonClick()
+                        }) {
+                            Text("Call Api")
+                        }
+                    }
+                    HandleTrigger(eventState = vm.trigger.state){context, event->
+                        when(event){
+                            is ToastEvent->{
+                                Toast.makeText(context, event.message, Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }
                 }
             }
         }
     }
+
+
+
 }
-
-
-
-@Composable
-fun MyPage(vm: MyViewModel = viewModel()){
-    HandleTrigger(eventState = vm.trigger.state){ c, d->
-        when(d){
-            is ToastEvent->{
-                Toast.makeText(c, d.message, Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-    Column(){
-        Text("Hello")
-    }
-}
-
-
-
 
 class MyViewModel: ViewModel(){
     val trigger = UITrigger()
-    init {
-        viewModelScope.launch(Dispatchers.IO) {
-            delay(3000)
-            viewModelScope.launch(Dispatchers.IO) {
-                trigger.post(ToastEvent("MyToast"))
-                trigger.post(ToastEvent("MyToast1"))
-                trigger.post(ToastEvent("MyToast2"))
-                trigger.post(ToastEvent("MyToast3"))
+    private suspend fun reallyCallApi(): Boolean {
+        delay(3000)
+        return false
+    }
+    fun onButtonClick(){
+        viewModelScope.launch {
+            val result = reallyCallApi()
+            if(!result){
+                showToast()
             }
         }
     }
+
+    private fun showToast() {
+        trigger.post(ToastEvent("Error"))
+    }
 }
 
-class ToastEvent(
-    val message: String,
-    shortTime: Boolean = true
-): UIEvent()
+class ToastEvent(val message: String, shortTime: Boolean = true): UIEvent()
